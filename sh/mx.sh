078a45ee955d8a5397efdb29a671da273c0d335e
GeezThatHurts <135326886+GeezThatHurts@users.noreply.github.com>
2024-02-21 11:36:18 +0700

    Update mx.sh

--- sh/mx.sh
+++ sh/mx.sh
@@ -9,11 +9,16 @@ db_forward=$domain
 db_reverse=db.rev
 
 #adding mx record
+echo "adding mx record..."
+sleep 2
 echo "$domain.	IN	MX 10	$point." >> /etc/bind/$db_forward
 
 #restarting bind9
+echo "restarting bind9"
+sleep 2
 systemctl restart bind9
 
 #done
 echo "congrats, now the $domain have an mx record pointing to $point"
+sleep 1
 echo "sasuga mignere san <3333"
