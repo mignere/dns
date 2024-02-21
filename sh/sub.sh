4e72d9c68024e1f0ff6fa2c4b1c1a5725517bf3d
GeezThatHurts <135326886+GeezThatHurts@users.noreply.github.com>
2024-02-21 11:37:08 +0700

    Update sub.sh

--- sh/sub.sh
+++ sh/sub.sh
@@ -7,12 +7,16 @@ read -p "what subdomain would you like to add? (example: mail/www/ftp/pma) : " s
 
 #editing configuration
 echo "editing configuration"
+sleep 2
 echo "$subdomain	IN	A	$ip" >> /etc/bind/$domain
 
 #restarting bind9
+echo "restarting bind9"
+sleep 2
 systemctl restart bind9
 
 #done
 echo "congrats, your $subdomain subdomain has been successfully made!"
+sleep 1
 echo "sasuga mignere san <333"
 
