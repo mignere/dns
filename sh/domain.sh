08f18c6b8628c3f9e086ba441aaeb490e181d103
GeezThatHurts <135326886+GeezThatHurts@users.noreply.github.com>
2024-02-24 16:02:34 +0700

    Update domain.sh

--- sh/domain.sh
+++ sh/domain.sh
@@ -10,11 +10,16 @@ read -p "input the ip address for this domain : " ip
 echo "this is the ip you will use for $domain : $ip"
 
 #making the raw reversed ip for user
-ip_reverse_raw="${ip#192.}"
-echo "this is your raw reverse ip : $ip_reverse_raw "
+ip_reverse_raw=$(echo $ip | awk -F '.' '{print $2"."$3"."$4}')
+echo "this is your raw reversed ip : $ip_reverse_raw "
+sleep 2
 
-read -p "please input the reversed ip, example = 168.1.2 = 2.1.168 : " ip_reverse
+ip_reverse=$(echo $ip | awk -F '.' '{print $4"."$3"."$2}')
 echo "this is your reversed ip : $ip_reverse"
+sleep 2
+
+#first octet for named.conf.local
+foctet=$(echo $ip | awk -F '.' '{print $1}')
 
 #user db forward lookup zone name and reverse lookup zone name
 db_forward=$domain
@@ -38,6 +43,17 @@ echo "$ip_reverse	IN	PTR	$domain." >> /etc/bind/$db_reverse
 #named.conf.local
 sed -i 's/localhost/'$domain'/g' /etc/bind/named.conf.local
 sed -i 's/db.local/'$db_forward'/g' /etc/bind/named.conf.local
+#checking for reverse zone file
+if grep -q "$foctet.in-addr.arpa" /etc/bind/named.conf.local; then
+    echo "reverse zone already made, proceeding..."
+    sleep 2
+else
+    echo "$foctet.in-addr.arpa doesnt found in the reverse zone file, making a new one..."
+    sleep 2
+    sed -n '15,18p' /etc/bind/named.conf.default-zones >> /etc/bind/named.conf.local
+    sed -i 's/db.127/'$db_reverse'/g' /etc/bind/named.conf.local
+    sed -i 's/127/'$foctet'/g' /etc/bind/named.conf.local
+fi
 
 #restarting bind9
 echo "restarting bind9"
